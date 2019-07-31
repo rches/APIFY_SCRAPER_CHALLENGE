396e026e0b4e02b4d3c332ad7b91c335a95adaf7
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-07-30 21:51:01 -0400

    add logic and data formatting to scraper

--- main.js
+++ main.js
@@ -15,7 +15,80 @@ Apify.main(async () => {
         // This page is executed for each request.
         // If request failes then it's retried 3 times.
         // Parameter page is Puppeteers page object with loaded page.
-        handlePageFunction: getEventData,
+        handlePageFunction: async function pageFunction(context) {
+            const { page, request, log } = context;
+        
+        
+            // find the description
+            const description = await page.evaluate(
+            () => document.querySelector("#tab-details").innerText
+          );
+            
+            //find the date    
+            const date = await page.evaluate(
+            () => document.querySelector("div.detail-top > div.detail-c2.left > div.dates").innerText
+          );
+        
+            //   find the place
+            const place = await page.evaluate(
+                () => document.querySelector("div.detail-top > div.detail-c2.left > div.adrs").innerText
+            );
+        
+            const addressArray = place.split(/[|,]/).map(function(item){
+                return item.trim();
+            });
+            const [street, city, statePostal] = [addressArray[0], addressArray[1], addressArray[2]]
+            const [state, postal] = statePostal.split(" ")
+        
+            //create an array of the information contained in the details div
+            const eventArray = await page.evaluate(
+            () => [...document.querySelectorAll('div.detail-top > div.detail-c2.left > div')].map(elem => elem.innerText)
+          );
+        
+            //filter for eventArray for specific information without a selector
+            const phone = eventArray.filter(info => 
+            info.includes('Phone:'))
+            .toString().replace('Phone: ', "");
+        
+            const time = eventArray.filter(info => 
+            info.includes('Times:'))
+            .toString().replace('Times: ', "");
+        
+            const contact = eventArray.filter(info => 
+            info.includes('Contact:'))
+            .toString().replace('Contact: ', "");
+        
+            const admission = eventArray.filter(info => 
+            info.includes('Admission:'))
+            .toString().replace('Admission: ', "");
+        
+            //   timestamp
+            const timestamp = Date.now();
+        
+        let event = {
+            "url": request.url,
+            "description": description,
+            "date": date,
+            "time": time,
+        
+            "place": {
+                "street": street,
+                "city": city,
+                "state": state,
+                "postal": postal
+            },
+        
+            "details": {
+                "contact": contact,
+                "phone": phone,
+                "admission": admission
+            },
+        
+            "timestamp": timestamp
+        }
+        
+        console.log(event);
+    },
 
         // If request failed 4 times then this function is executed.
         handleFailedRequestFunction: async ({ request }) => {
@@ -28,19 +101,5 @@ Apify.main(async () => {
     
 });
 
-
-
-const getEventData = async ({ page, request }) => {
-
-    // Function to get data from page
-    const title = await page.title();
-    const posts = await page.$$('.athing');
-
-    console.log(`Page ${request.url} succeeded and it has ${posts.length} posts.`);
-
-
     // Log data (util is a tool that nicely formats objects in the console)
-    console.log(util.inspect(title, false, null));
-}
-
-
+    // console.log(util.inspect(url, false, null));
