9681293cd85a7ed28456dec0e4ec5bd6323d4d79
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-08-01 23:12:43 -0400

    adding updated scraper logic and storage

--- main.js
+++ main.js
@@ -1,110 +1,141 @@
 const Apify = require('apify');
-const util = require('util');
-
 
 Apify.main(async () => {
+    const sources = ['https://www.visithoustontexas.com/events/']
 
-    // Get queue and enqueue first url.
-    const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest(new Apify.Request({ url: 'https://www.visithoustontexas.com/event/zumba-in-the-plaza/59011/' }));
+const requestList = await Apify.openRequestList('events', sources);
+const requestQueue = await Apify.openRequestQueue();
 
-    // Create crawler.
     const crawler = new Apify.PuppeteerCrawler({
+        maxRequestsPerCrawl: 50,
+        requestList,
         requestQueue,
-
-        // This page is executed for each request.
-        // If request failes then it's retried 3 times.
-        // Parameter page is Puppeteers page object with loaded page.
-        handlePageFunction: async function pageFunction(context) {
+        handlePageFunction: async function(context) {
             const { page, request, log } = context;
-        
-        
-            // find the description
-            const description = await page.evaluate(
-            () => document.querySelector("#tab-details").innerText
-          );
-            
-            //find the date    
-            const date = await page.evaluate(
-            () => document.querySelector("div.detail-top > div.detail-c2.left > div.dates").innerText
-          );
-        
-            //   find the place
-            const place = await page.evaluate(
-                () => document.querySelector("div.detail-top > div.detail-c2.left > div.adrs").innerText
-            );
-        
-            const addressArray = place.split(/[|,]/).map(function(item){
-                return item.trim();
-            });
-            const [street, city, statePostal] = [addressArray[0], addressArray[1], addressArray[2]]
-            const [state, postal] = statePostal.split(" ")
-        
-            //create an array of the information contained in the details div
-            const eventArray = await page.evaluate(
-            () => [...document.querySelectorAll('div.detail-top > div.detail-c2.left > div')].map(elem => elem.innerText)
-          );
-        
-            //filter for eventArray for specific information without a selector
-            const recurring = eventArray.filter(info => 
-            info.includes('Recurring'))
-            .toString();
-            
-            const phone = eventArray.filter(info => 
-            info.includes('Phone:'))
-            .toString().replace('Phone: ', "");
-        
-            const time = eventArray.filter(info => 
-            info.includes('Times:'))
-            .toString().replace('Times: ', "");
-        
-            const contact = eventArray.filter(info => 
-            info.includes('Contact:'))
-            .toString().replace('Contact: ', "");
-        
-            const admission = eventArray.filter(info => 
-            info.includes('Admission:'))
-            .toString().replace('Admission: ', "");
-        
-            //   timestamp
-            const timestamp = Date.now();
-        
-        let event = {
-            "url": request.url,
-            "description": description,
-            "date": date,
-            "time": time,
-            "recurring": recurring,
-        
-            "place": {
-                "street": street,
-                "city": city,
-                "state": state,
-                "postal": postal
-            },
-        
-            "details": {
-                "contact": contact,
-                "phone": phone,
-                "admission": admission
-            },
-        
-            "timestamp": timestamp
-        }
-        
-        console.log(event);
-    },
+            console.log(`Processing ${request.url}`);
 
-        // If request failed 4 times then this function is executed.
-        handleFailedRequestFunction: async ({ request }) => {
-            console.log(`Request ${request.url} failed 4 times`);
+            if (request.userData.eventPage) {
+                // find the description
+                const description = await page.evaluate(
+                () => document.querySelector("#tab-details").innerText
+            );
+                
+                //find the date    
+                const date = await page.evaluate(
+                () => document.querySelector("div.detail-top > div.detail-c2.left > div.dates").innerText
+            );
+
+                //   find the place
+                const place = await page.evaluate(
+                    () => document.querySelector("div.detail-top > div.detail-c2.left > div.adrs").innerText
+                );
+
+                const addressArray = place.split(/[|,]/).map(function(item){
+                    return item.trim();
+                });
+                const [street, city, statePostal] = [addressArray[0], addressArray[1], addressArray[2]]
+                const [state, postal] = statePostal.split(" ")
+
+                //create an array of the information contained in the details div
+                const eventArray = await page.evaluate(
+                () => [...document.querySelectorAll('div.detail-top > div.detail-c2.left > div')].map(elem => elem.innerText)
+            );
+
+                //filter for eventArray for specific information without a selector
+                const recurring = eventArray.filter(info => 
+                info.includes('Recurring'))
+                .toString();
+                
+                const phone = eventArray.filter(info => 
+                info.includes('Phone:'))
+                .toString().replace('Phone: ', "");
+
+                const time = eventArray.filter(info => 
+                info.includes('Times:'))
+                .toString().replace('Times: ', "");
+
+                const contact = eventArray.filter(info => 
+                info.includes('Contact:'))
+                .toString().replace('Contact: ', "");
+
+                const admission = eventArray.filter(info => 
+                info.includes('Admission:'))
+                .toString().replace('Admission: ', "");
+
+                //   timestamp
+                const timestamp = Date.now();
+
+                let event = {
+                    "url": request.url,
+                    "description": description,
+                    "date": date,
+                    "time": time,
+                    "recurring": recurring,
+
+                    "place": {
+                        "street": street,
+                        "city": city,
+                        "state": state,
+                        "postal": postal
+                    },
+
+                    "details": {
+                        "contact": contact,
+                        "phone": phone,
+                        "admission": admission
+                    },
+
+                    "timestamp": timestamp
+                }
+                console.log('Events: ', event )
+            }
+
+            if (request.userData.eventsPage) {
+                // A function to be evaluated by Puppeteer within the browser context.
+                const getMoreLinks = $links => {
+                    const data = [];
+
+                    // Find the href of each header for each event
+                    $links.forEach($link => {
+                        data.push({
+                            url: $link.querySelector('.title a').href,
+                        });
+                    });
+
+                    return data;
+                };
+                const data = await page.$$eval('.eventItem', getMoreLinks);
+
+                // Store the results to the default dataset.
+                await Apify.pushData(data);
+
+            }
+
+            if (!request.userData.eventsPage) {
+                await Apify.utils.enqueueLinks({
+                    page,
+                    requestQueue,
+                    pseudoUrls: ['https://www.visithoustontexas.com/events/?page=[\d+]'],
+                    transformRequestFunction: req => {
+                        req.userData.eventsPage = true;
+                        return req;
+                    },
+                });
+            }
+
+            if (!request.userData.eventPage) {
+                await Apify.utils.enqueueLinks({
+                    page,
+                    requestQueue,
+                    selector: '.title a',
+                    transformRequestFunction: req => {
+                        req.userData.eventPage = true;
+                        return req;
+                    },
+                });
+            }
         },
     });
 
-    // Run crawler.
     await crawler.run();
-    
 });
-
-    // Log data (util is a tool that nicely formats objects in the console)
-    // console.log(util.inspect(url, false, null));
